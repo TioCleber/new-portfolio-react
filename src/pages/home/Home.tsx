5fee7d4f490c935d41ee0d45af539704f9299b72
Cleber zacher <cleber.zacher@acct.global>
2023-08-01 13:33:18 -0300

    feat: added slider swiper lib

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,16 @@
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import TypeWriter from "../../components/typewriter/TypeWriter";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
 
 const Home = () => {
   const text = [
     "Atualmente estou atuando como Desenvolvedor Pleno",
-    "Testando typewriter",
+    "Possuo experiências com:",
+    "JavaScript, JQuery, ReactJs, NodeJs, ExpressJs, Graphql, Git e muito mais...",
   ];
 
   return (
@@ -34,6 +39,89 @@ const Home = () => {
 
         <section id="about">
           <h1>Sobre</h1>
+        </section>
+
+        <section id="projects">
+          {
+            <Swiper
+              pagination={{
+                type: "fraction",
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div>
+                  <h2>Pousada Secreta</h2>
+
+                  <div>
+                    <a
+                      href="https://pousada-secreta.netlify.app/"
+                      target="_blank"
+                    >
+                      <img
+                        src="https://portifolio-zac.netlify.app/assets/pousada.png"
+                        alt=""
+                        loading="lazy"
+                        width={1920}
+                        height={937}
+                        style={{ height: "auto" }}
+                      />
+                    </a>
+                  </div>
+
+                  <div>
+                    <p>
+                      Projeto concluído feito em meu início de aprendizado com{" "}
+                      <strong>HTML</strong>,<strong>CSS</strong> e com
+                      implementação de <strong>JS</strong> somente para links
+                      suaves, É um projeto que eu comecei pela plataforma da{" "}
+                      <strong>DevMedia</strong> onde tenho aprendido bastante
+                      conteúdos relacionados ao Front.
+                    </p>
+
+                    <p>
+                      É sobre uma pousada fictícia onde são apresentados os
+                      tipos de quartos para clientes que se interessarem e
+                      contatarem.
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div>
+                  <h2>Cm-Life</h2>
+                  <div>
+                    <a href="https://cmlife.netlify.app/" target="_blank">
+                      <img
+                        src="https://portifolio-zac.netlify.app/assets/cmlife.png"
+                        alt=""
+                        loading="lazy"
+                        width={1920}
+                        height={937}
+                        style={{ height: "auto" }}
+                      />
+                    </a>
+                  </div>
+                  <div>
+                    <p>
+                      Projeto concluído feito com <strong>HTML</strong>,{" "}
+                      <strong>CSS</strong> e com implementação de
+                      <strong>JS</strong> somente para links suaves, É um
+                      projeto que eu comecei pela plataforma da
+                      <strong>DevMedia</strong> também.
+                    </p>
+
+                    <p>
+                      É sobre uma clínica fictícia apresentando os serviços
+                      disponíveis para pessoas de qualquer idade.
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          }
         </section>
       </main>
 
