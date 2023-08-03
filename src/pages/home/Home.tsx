b4bccfbbe0f1161988b4fbd82f47c25996c53880
Cleber zacher <cleber.zacher@acct.global>
2023-08-03 00:28:29 -0300

    feat: added lazy load to slider

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,18 @@
+import { lazy, Suspense } from "react";
+
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
-import Slider from "../../components/external-components/slider/Slider";
+
 import TypeWriter from "../../components/external-components/typewriter/TypeWriter";
 import Projects from "../../components/projects/Projects";
 import {
   FIRST_PROJECT,
   SECOND_PROJECT,
 } from "../../constants/dangerouslySetInnerHTML";
+
+const Slider = lazy(
+  () => import("../../components/external-components/slider/Slider")
+);
 
 const Home = () => {
   const text = [
@@ -39,32 +45,32 @@ const Home = () => {
           </div>
         </section>
 
-        <section id="about">
+        <section id="about" style={{ height: 500 }}>
           <h1>Sobre</h1>
         </section>
 
         <section id="projects">
-          <Slider
-            sliderElement={[
-              <Projects
-                title="Pousada Secreta"
-                imageLink="https://pousada-secreta.netlify.app/"
-                imageSrc="https://portifolio-zac.netlify.app/assets/pousada.png"
-                dangerouslySetInnerHTML={FIRST_PROJECT}
-              />,
-              <Projects
-                title="Cm-Life"
-                imageLink="https://cmlife.netlify.app/"
-                imageSrc="https://portifolio-zac.netlify.app/assets/cmlife.png"
-                dangerouslySetInnerHTML={SECOND_PROJECT}
-              />,
-            ]}
-          />
+          <Suspense>
+            <Slider
+              sliderElement={[
+                <Projects
+                  title="Pousada Secreta"
+                  imageLink="https://pousada-secreta.netlify.app/"
+                  imageSrc="https://portifolio-zac.netlify.app/assets/pousada.png"
+                  dangerouslySetInnerHTML={FIRST_PROJECT}
+                />,
+                <Projects
+                  title="Cm-Life"
+                  imageLink="https://cmlife.netlify.app/"
+                  imageSrc="https://portifolio-zac.netlify.app/assets/cmlife.png"
+                  dangerouslySetInnerHTML={SECOND_PROJECT}
+                />,
+              ]}
+            />
+          </Suspense>
         </section>
 
-        <section id="skills">
-
-        </section>
+        <section id="skills"></section>
       </main>
 
       <Footer />
